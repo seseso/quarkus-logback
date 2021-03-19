57b5ca7a9262d8aa9207a8a63a8b59ce6f4ed7ff
Sergio Sousa <sergio.sousa@carrotincentives.com>
2021-03-19 11:55:14 +0000

    Initialize logback in a safe manner

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -4,13 +4,18 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Path("/hello-resteasy")
 public class GreetingResource {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(GreetingResource.class);
+    
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        LOGGER.info("hello invoked");
         return "Hello RESTEasy";
     }
 }
--- src/main/java/org/acme/Init.java
+++ src/main/java/org/acme/Init.java
@@ -9,6 +9,7 @@ import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.joran.JoranConfigurator;
 import ch.qos.logback.core.joran.spi.JoranException;
 import io.quarkus.runtime.StartupEvent;
+import java.io.InputStream;
 import java.util.Date;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
@@ -20,40 +21,39 @@ import org.slf4j.LoggerFactory;
  *
  * @author seseso
  */
-@ApplicationScoped
+@Singleton
 public class Init {
     
     private static final Logger LOGGER = LoggerFactory.getLogger(Init.class);
-
-    static {
-        ((LoggerContext) LoggerFactory.getILoggerFactory()).stop();
-    }
+//
+//    static {
+//        ((LoggerContext) LoggerFactory.getILoggerFactory()).stop();
+//    }
     
     public void onStart(@Observes StartupEvent ev) {
         
-        LOGGER.info("Startup in progress...");
-        
-        
-        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
-//        StatusPrinter.print(lc);
-        
         try {
+            LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+            
             JoranConfigurator jc = new JoranConfigurator();
             jc.setContext(lc);            
             lc.reset(); // override default configuration
             // inject the name of the current application as "application-name"
             // property of the LoggerContext
-            lc.putProperty("application-name", "code-with-quarkus");
-            lc.putProperty("LOG_FILE_NAME", "code-with-quarkus-" + new Date().getTime() + ".log");
-            lc.putProperty("DEFAULT_LOG_FOLDER", "./log");
-            
+//            lc.putProperty("application-name", "c3-integration-cmd");
+//            lc.putProperty("LOG_FILE_NAME", "c3-integration-cmd-" + df.format(new Date()) + ".log");
+//            lc.putProperty("DEFAULT_LOG_FOLDER", config.getValue("Inbound.Temp.Folder", String.class));
             
             jc.doConfigure(getClass().getResourceAsStream("/logback.xml"));
+            LOGGER.info("Logback was configured with sucess");
             
 //            lc.stop();
 //            StatusPrinter.print(lc);
         } catch(JoranException e) {
             LOGGER.warn(null, e);
+            e.printStackTrace();
+        } catch(ClassCastException e) {
+            LOGGER.error("Could not configure Logback. Is is quarkus:dev fault? ERROR: {}", e.getMessage());
         }
         
         LOGGER.info("Initializing app...");
